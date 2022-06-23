f46a5b58f3fd3b4600e122d5b4ab73f296cf60c8
KWalkerNNK <kwalkernnk@gmail.com>
2022-06-23 14:49:50 +0700

    love My Hanh

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const morgan = require('morgan')
-const { engine } = require('express-handlebars')
+const {engine} = require('express-handlebars')
 const path = require('path')
 
 const app = express()
 const port = 3000
+
+const route = require('./routes/index')
 
 //Hiển thị ảnh
 app.use(express.static(path.join(__dirname,'public')))
 
 //HTTP Logger // Hiển thị console.log
 app.use(morgan('combined'))
+
+//Middleware for post method
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 //Template engine
 app.engine('hbs', engine({
@@ -20,13 +26,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname,'resources/views'))
 
-app.get('/trangchu', (req, res) => {
-  res.render('home')
-})
-
-app.get('/tintuc', (req, res) => {
-  res.render('news')
-})
+//Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
