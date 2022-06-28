5bfb98379fa0fc95a6a62c4d520690db997cda3a
KWalkerNNK <kwalkernnk@gmail.com>
2022-06-28 11:35:13 +0700

    💤

--- src/app/controllers/MovieController.js
+++ src/app/controllers/MovieController.js
@@ -4,8 +4,23 @@ class MovieController {
     show(req, res, next) {
         Song.findOne({ slug: req.params.slug })
             .lean()
-            .then(song => res.render('movies/show', { song }))
-            .catch(next)
+            .then((song) => res.render('movies/show', { song }))
+            .catch(next);
+    }
+
+    // GET /movies/create
+    create(req, res, next) {
+        res.render('movies/create');
+    }
+
+    // POST
+    store(req, res, next) {
+        const formData = req.body;
+        formData.img = `http://img.youtube.com/vi/${formData.url}/sddefault.jpg`;
+        const song = new Song(formData);
+        song.save()
+            .then(() => res.redirect('/'))
+            .catch(next);
     }
 }
 module.exports = new MovieController();
--- src/app/models/Song.js
+++ src/app/models/Song.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
 
 const Schema = mongoose.Schema;
+
+mongoose.plugin(slug);
 
 const Song = new Schema({
     name: { type: String, maxLength: 255 },
     url: { type: String, maxLength: 255 },
     img: { type: String, maxLength: 255 },
     author: { type: String, maxLength: 70 },
-    slug: { type: String, maxLength: 255},
+    slug: { type: String, slug: 'name', unique: true },
 });
 
 module.exports = mongoose.model('Song', Song);
